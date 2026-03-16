cd10bec78b22ef6cf6c82dfce24a77a8d548cb50
Killian <leclainche.killian03@gmail.com>
2026-03-16 14:05:34 +0100

    Création de PanierTest

--- TP Junit/src/test/java/fr/boutique/PanierTest.java
+++ TP Junit/src/test/java/fr/boutique/PanierTest.java
@@ -1,5 +1,50 @@
 package fr.boutique;
 
-public class PanierTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class PanierTest {
+
+    @Test
+    void ajouterArticleDeitAugmenterLeNombreDeArticles() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo bleu", 1.50);
+        // Agir
+        panier.ajouterArticle(article, 2);
+        // Affirmer
+        assertEquals(1, panier.nombreArticles());
+    }
+
+    @Test
+    void calculerTotalDoitRetournerLaSommeDessousTotaux() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article1 = new Article("REF-001", "Stylo bleu", 1.50);
+        Article article2 = new Article("REF-002", "Cahier", 1.50);
+        // Agir
+        panier.ajouterArticle(article1, 3);
+        panier.ajouterArticle(article2, 3);
+        // Affirmer
+        assertEquals(9.00, panier.calculerTotal(), 0.001);
+    }
+
+    @Test
+    void panierVideDoitRetournerEstVideEgalTrue() {
+        // Arranger
+        Panier panier = new Panier();
+        // Affirmer
+        assertTrue(panier.estVide());
+    }
+
+    @Test
+    void panierAvecArticlesNeDoitPasEtreVide() {
+        // Arranger
+        Panier panier = new Panier();
+        Article article = new Article("REF-001", "Stylo bleu", 1.50);
+        // Agir
+        panier.ajouterArticle(article, 1);
+        // Affirmer
+        assertFalse(panier.estVide());
+    }
 }
